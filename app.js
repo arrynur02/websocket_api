c07c57ca56431b0f78d3abff27c7246ffee6c5d2
arrynur02 <armynooz@gmail.com>
2022-06-15 00:31:44 +0700

    updates

--- app.js
+++ app.js
@@ -4,17 +4,13 @@ const path = require('path');
 const https = require('https');
 const http = require('http');
 const cors = require('cors');
+const{ WebSocketServer } = require('ws');
 const bodyParser = require('body-parser');
 const { Server } = require('socket.io');
 
 require('dotenv').config({ path:'.env' });
 
-const server = https.createServer(app);
-const io = new Server(server, { 
-    cors: {
-      origin:'https://qr.buildercorp.id'
-    } 
-});
+const server = http.createServer(app);
 
 app.use(cors({
     origin:["https://qr.buildercorp.id","https://103.134.152.9:8080","*"]
@@ -32,33 +28,46 @@ app.set('views',path.join(__dirname,'views'));
 
 app.set('view engine','ejs');
 
-io.on("connection", (socket) => {
-  // console.log('a user connected!');
-  socket.on('mm',(data) =>{
-    // io.emit('event', data);
-    console.log(data);
+const wss = new WebSocketServer({ server:server });
+
+function getId(length) {
+  var result='';
+  var characters='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var characterLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characterLength))
+  }
+  return result;
+}
+
+function msgWebsocket(id, data, status){
+  return JSON.stringify({
+    type: "message",
+    id:id,
+    text: data,
+    status:'success',
+    date: Date.now()
   });
+}
+
+wss.on('connection', function connection(ws, req) {
+  ws.on('message', function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      // console.log(client);
+      if (client.readyState) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+
+  ws.send(msgWebsocket(getId(15),'connect user..',200));
 });
 
-// io.use((socket, next) => {
-  
-//   const token = socket.handshake.auth.token;
-
-//   if (token!=='123#'){
-//     next(new Error('error token!'));
-//   } else {
-//     next();
-//   }
-// });
-// io.use((socket, next) => {
-//   next();
-// });
-
 app.get('/',(req, res) => {
-  let status = req.query.status;
-  res.render('index',{
-      status:status
-    });
+  let data = {
+    uid:getId(15)
+  }
+  res.render('apis', data);
 });
 
 app.get('/apisocks',(req, res) => {
@@ -71,26 +80,12 @@ app.get('/apisocks',(req, res) => {
     }
 });
 
-app.post('/auth',(req, res) => {
-    let token = req.body.data_input_tokens;
-    if (token===process.env.TOKEN){
-      res.redirect('/apisocks?token='+token);
-    } else {
-      res.redirect('/?status=error');
-    }
-});
-
 app.post('/sending_api',(req, res) => {
   if (req.query.token===process.env.TOKEN){
     let text={
       text:'success',
       data:req.body.data_input
     };
-    io.on("connection", (socket) => {
-      // console.log('a user connected!');
-      socket.emit('ondata', text);
-    });
-    console.log(req.body.data_input);
     res.redirect('/apisocks');
   } else {
     res.status(404).json({
@@ -104,5 +99,5 @@ app.post('/sending_api',(req, res) => {
 var port = process.env.PORT||5000;
 
 server.listen(port,() => {
-  console.log('listen at server.. http://localhost:', port);
+  console.log('listen server at http://localhost:'+port);
 });
